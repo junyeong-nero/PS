2752cc7ea1e2b0c5bb79f32b6e7c2ad9b9fff867
junyeong-nero <magicwho@unist.ac.kr>
2024-12-10 17:52:10 +0900

    [solve] 24/12/10

---
+++ leetcode/2981.py
@@ -0,0 +1,17 @@
+from itertools import defaultdict
+
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        d = defaultdict(list)
+        text, size = '', 0
+
+        for i in range(len(s)):
+            if text == s[i]:
+                size += 1
+            else:
+                text = s[i]
+                size = 1
+            d[text].append(size)
+        
+        arr = [sorted(x, reverse=True)[2] for x in d.values() if len(x) >= 3]
+        return max(arr) if arr else -1
