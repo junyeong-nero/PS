f2b4a40f259c4bb29d0b26750676e7ff4e194bbc
junyeong-nero <magicwho@unist.ac.kr>
2024-12-11 11:58:56 +0900

    [solve] another solution 24/12/11

--- leetcode/2779.py
+++ leetcode/2779.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         events = []
@@ -16,4 +18,21 @@ class Solution:
             current += change
             max_beauty = max(max_beauty, current)
         
+        return max_beauty
+
+    # sorting 
+    def maximumBeauty(self, nums: list[int], k: int) -> int:
+        nums.sort()
+        left = 0
+        max_beauty = 0
+
+        # Iterate through the array with the right pointer
+        for right in range(len(nums)):
+            # Move the left pointer to maintain the valid range
+            while nums[right] - nums[left] > 2 * k:
+                left += 1
+            # Update the maximum beauty based on the current range
+            # We do not add 1 here as right is already pointing to one position beyond the valid range.
+            max_beauty = max(max_beauty, right - left + 1)
+
         return max_beauty
