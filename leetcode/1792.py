6db42abfa40ab35b7e5bee098412cadfee360072
junyeong-nero <magicwho@unist.ac.kr>
2024-12-15 17:50:47 +0900

    [update] 24/12/15

--- leetcode/1792.py
+++ leetcode/1792.py
@@ -1,17 +1,15 @@
-from heapq import heappush, heappop
+from heapq import heappush, heappop, heapify
 
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
         n = len(classes)
-        arr = []
-        for i in range(n):
-            tar = classes[i]
-            grad = (tar[0] + 1) / (tar[1] + 1) - tar[0] / tar[1]
-            heappush(arr, (-grad, tar[0], tar[1]))
+        classes = [(tar[0] / tar[1] - (tar[0] + 1) / (tar[1] + 1), tar[0], tar[1]) for tar in classes]
+        heapify(classes)
 
         for _ in range(extraStudents):
-            tar = heappop(arr)
-            grad = (tar[1] + 2) / (tar[2] + 2) - (tar[1] + 1) / (tar[2] + 1)
-            heappush(arr, (-grad, tar[1] + 1, tar[2] + 1))
+            _, a, b = heappop(classes)
+            a, b = a + 1, b + 1, 
+            grad = a / b - (a + 1) / (b + 1)
+            heappush(classes, (grad, a, b))
 
-        return sum([x[1] / x[2] for x in arr]) / n
+        return sum([x[1] / x[2] for x in classes]) / n
