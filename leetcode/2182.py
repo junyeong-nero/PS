4542ba6e46d95d9e6efdeb8b3f06b13f42e33e47
junyeong-nero <magicwho@unist.ac.kr>
2024-12-17 15:24:07 +0900

    [update] 24/12/17

--- leetcode/2182.py
+++ leetcode/2182.py
@@ -1,36 +1,43 @@
-from itertools import Counter
+from collections import Counter
 
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
-        s = Counter(s)
-        keys = sorted(s.keys(), reverse=True)
-        n = len(keys)
-        temp = ''
+        # Count the frequency of each character
+        char_count = Counter(s)
+        # Sort characters by their lexicographical order in descending order
+        sorted_keys = sorted(char_count.keys(), reverse=True)
+        
+        result = []
+        n = len(sorted_keys)
+
         for i in range(n - 1):
-            check = True
-            key = keys[i]
-            while s[key] > repeatLimit:
-                new_key = ''
+            key = sorted_keys[i]
+            while char_count[key] > repeatLimit:
+                # Find the next available character
+                next_key = ''
                 for j in range(i + 1, n):
-                    if s[keys[j]] > 0:
-                        new_key = keys[j]
+                    if char_count[sorted_keys[j]] > 0:
+                        next_key = sorted_keys[j]
                         break
 
-                if new_key == '':
-                    check = False
+                # If no valid next character exists, break the loop
+                if not next_key:
                     break
-                
-                s[key] -= repeatLimit
-                temp += key * repeatLimit   
-                s[new_key] -= 1
-                temp += new_key
-            
-            if check:
-                temp += key * s[key]
-                s[key] = 0
-                
 
-        for i in range(n):
-            temp += keys[i] * min(s[keys[i]], repeatLimit)
-        return temp
-        
+                # Add the current character up to the repeat limit
+                result.append(key * repeatLimit)
+                char_count[key] -= repeatLimit
+
+                # Add one instance of the next character
+                result.append(next_key)
+                char_count[next_key] -= 1
+
+            # Add remaining instances of the current character (if any)
+            result.append(key * char_count[key])
+            char_count[key] = 0
+
+        # Add any remaining characters from the last position onward
+        for key in sorted_keys:
+            result.append(key * min(char_count[key], repeatLimit))
+
+        return ''.join(result)
