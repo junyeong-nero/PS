ca9c9c0b6c9fe66bb08f7477cf520f30069c9105
junyeong-nero <magicwho@unist.ac.kr>
2024-12-26 13:14:51 +0900

    [solve] 24/12/26

---
+++ leetcode/494.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        d = defaultdict(dict)
+        
+        def dfs(idx, tar):
+            if tar in d[idx]:
+                return d[idx][tar]
+            if idx >= len(nums):
+                return 1 if tar == 0 else 0
+            
+            temp = dfs(idx + 1, tar + nums[idx]) + dfs(idx + 1, tar - nums[idx])
+            d[idx][tar] = temp
+            return temp
+
+        return dfs(0, target)
