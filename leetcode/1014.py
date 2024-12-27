6f2687881e57d3980ff24f229995201cc708b0cf
junyeong-nero <magicwho@unist.ac.kr>
2024-12-27 12:48:01 +0900

    [solve] 24/12/27

--- leetcode/1014.py
+++ leetcode/1014.py
@@ -1,20 +1,7 @@
 class Solution:
-    def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        n = len(values)
-        res = 0
-        values = sorted([(values[i], i) for i in range(n)], reverse=True)
-        print(values)
-        # i - j < n
-        for i in range(n - 1):
-            temp = -n
-            a = values[i]
-            for j in range(i + 1, n):
-                b = values[j]
-                val = a[0] + b[0] - abs(a[1] - b[1])
-                if val >= temp:
-                    temp = val
-                else:
-                    break
-            res = max(res, temp)
-
+    def maxScoreSightseeingPair(self, A):
+        cur = res = 0
+        for a in A:
+            res = max(res, cur + a)
+            cur = max(cur, a) - 1
         return res
