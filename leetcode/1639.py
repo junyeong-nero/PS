8e9607b1898aa38b59054b6d358bf4c50267e2cf
junyeong-nero <magicwho@unist.ac.kr>
2024-12-29 11:50:24 +0900

    [solve] 1639.py (2)

--- leetcode/1639.py
+++ leetcode/1639.py
@@ -1,31 +1,35 @@
 from collections import defaultdict
 from typing import List
 from functools import cache
-from bisect import bisect_right
 
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
         n, MOD = len(target), 10**9 + 7
-        d = defaultdict(dict)
+        m = len(words[0])
+        freq = [defaultdict(int) for _ in range(m)]
 
+        # 각 열의 문자의 빈도수를 저장
         for word in words:
             for i, c in enumerate(word):
-                if i not in d[c]:
-                    d[c][i] = 0
-                d[c][i] += 1
-        # print(d)
+                freq[i][c] += 1
 
         @cache
-        def dfs(index, k):
+        def dfs(index, pos):
             if index == n:
                 return 1
+            if pos >= m:
+                return 0
+            
+            char = target[index]
             temp = 0
-            c = target[index]
-            for i in d[c].keys():
-                if i <= k:
-                    continue
-                temp += d[c][i] * dfs(index + 1, i)
-            temp = temp % MOD
-            return temp
 
-        return dfs(0, -1)
+            # 현재 열에서 문자를 선택하는 경우
+            if char in freq[pos]:
+                temp += freq[pos][char] * dfs(index + 1, pos + 1)
+            
+            # 현재 열을 건너뛰는 경우
+            temp += dfs(index, pos + 1)
+
+            return temp % MOD
+
+        return dfs(0, 0)
