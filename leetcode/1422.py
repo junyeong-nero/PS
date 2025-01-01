771b4c7c6d2ca3765f6a6609689f1605fe831dbd
junyeong-nero <magicwho@unist.ac.kr>
2025-01-01 14:45:48 +0900

    [solve] 1422.py

---
+++ leetcode/1422.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        right, left = 0, s.count('1')
+        # print(right, left)
+        temp = 0
+        for i in range(len(s) - 1):
+            if s[i] == '0':
+                right += 1
+            if s[i] == '1':
+                left -= 1
+            temp = max(temp, right + left)
+
+        return temp
