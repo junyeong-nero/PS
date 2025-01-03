fcebd88502869f37b4961c138ded9266b47e2fc5
junyeong-nero <magicwho@unist.ac.kr>
2025-01-03 23:37:54 +0900

    [solve] 2270.py

---
+++ leetcode/2270.py
@@ -0,0 +1,12 @@
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        left = 0
+        right = sum(nums)
+        res = 0
+        for num in nums[:-1]:
+            left += num
+            right -= num
+            if left >= right:
+                res += 1
+        
+        return res
