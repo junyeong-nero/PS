12e2eb67ba7b53078650aaee9e2f990ef404c6c7
junyeong-nero <magicwho@unist.ac.kr>
2025-01-13 22:26:29 +0900

    [solve] 3223.py

---
+++ leetcode/3223.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        counter = Counter(s)
+        res = 0
+        for val in counter.values():
+            temp = val % 2
+            res += (2 if temp == 0 else temp)
+        return res
