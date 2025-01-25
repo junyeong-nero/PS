f43e39d19bbb524266ca5b3997ae84c321be05fb
junyeong-nero <magicwho@unist.ac.kr>
2025-01-25 23:47:32 +0900

    [fail] 2948.py

--- 2948.py
+++ 2948.py
@@ -1,17 +1,27 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         n = len(nums)
-        i = 0
-        while i < n:
-            idx = i
-            for j in range(i + 1, n):
-                diff = nums[i] - nums[j]
-                if diff > 0 and diff <= limit:
-                    if nums[j] < nums[idx]:
-                        idx = j
-            if idx != i:
-                nums[i], nums[idx] = nums[idx], nums[i]
-                i -= 1
-            i += 1
+        sort = sorted(nums)
+        res = []
 
-        return nums
+        @cache
+        def dfs(index):
+            if index == n:
+                return True
+
+            temp = False
+            for i in range(n):
+                if sort[i] == -1 or abs(sort[i] - nums[index]) > limit:
+                    continue
+                
+                origin = sort[i]
+                sort[i] = -1
+                if dfs(index + 1):
+                    temp = True
+                    res.append(origin)
+                    break
+                sort[i] = origin
+            return temp
+
+        dfs(0)
+        return res[::-1]
