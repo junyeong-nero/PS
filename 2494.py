8e6271e8150107072753b09d67d0ebabd5010cc1
junyeong-nero <magicwho@unist.ac.kr>
2025-01-30 18:30:36 +0900

    [fail] 2494.py with BFS

--- 2494.py
+++ 2494.py
@@ -5,19 +5,35 @@ class Solution:
         for u, v in edges:
             tree[u].append(v)
             tree[v].append(u)
-        print(tree)
+        # print(tree)
         
         visited = {}
-        def dfs(cur, level=0):
-            if cur in visited:
-                if level < visited[cur]:
-                    return float('inf') # cycle detected
-                return visited[cur]
-            visited[cur] = level
-            temp = level
-            for node in tree[cur]:
-                temp = max(temp, dfs(node, level + 1))
-            return temp + 1
+        def bfs(cur):
+            q = deque([cur])
+            visited = {}
+            level = 0
+            while q:
+                # print(q)
+                for _ in range(len(q)):
+                    tar = q.popleft()
+                    visited[tar] = level
+                    for node in tree[tar]:
+                        if node in visited:
+                            if abs(visited[node] - level) != 1:
+                                level = float('inf')
+                                break
+                        else:
+                            q.append(node)
+                level += 1
+            # print(cur, level, visited)
+            return level               
 
-        res = dfs(1, 0)
-        return -1 if res == float('inf') else res
+        res = -1
+        for i in range(1, n + 1):
+            visited = {}
+            temp = bfs(i)
+            if temp == float('inf'):
+                continue
+            res = max(res, temp)
+
+        return res
