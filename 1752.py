c80a434fc673bca04679b4b143c5ae936a16aa0f
junyeong-nero <magicwho@unist.ac.kr>
2025-02-02 21:18:51 +0900

    solve 1752

---
+++ 1752.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        """
+        Checks if a given list of integers is a rotated sorted array.
+
+        Args:
+            nums: A list of integers.
+
+        Returns:
+            True if the list is a rotated sorted array, False otherwise.
+        """
+        
+        inversions = 0  # Count of non-decreasing transitions
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                inversions += 1
+
+        # A rotated sorted array can have at most one "inversion"
+        # Check for wrap-around case where first element is greater than last
+        return (inversions == 1 and nums[0] >= nums[-1]) or inversions == 0
