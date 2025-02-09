e9b5dccc5d19c89b8c6e57bb3c637055561a4203
junyeong-nero <magicwho@unist.ac.kr>
2025-02-09 19:10:45 +0900

    optimize 2364

--- leetcode/2364.py
+++ leetcode/2364.py
@@ -1,10 +1,24 @@
+from collections import Counter
 
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
-        n = len(nums)
-        res = 0
-        for i in range(n):
-            for j in range(i + 1, n):
-                if j - i != nums[j] - nums[i]:
-                    res += 1
-        return res
+        """Counts the number of "bad" pairs in the given list.
+
+        A pair (i, j) is considered "bad" if i < j and nums[i] - i != nums[j] - j.
+
+        Args:
+            nums: A list of integers.
+
+        Returns:
+            The number of bad pairs.
+        """
+
+        count = 0  # Initialize the count of bad pairs
+        diff_counts = Counter()  # Use a Counter to store the frequency of differences (value - index)
+
+        for index, value in enumerate(nums):
+            difference = value - index  # Calculate the difference between the value and index
+            count += index - diff_counts[difference]  # Add to the count: current index - number of prior occurrences of this difference
+            diff_counts[difference] += 1  # Increment the count of this difference
+
+        return count
