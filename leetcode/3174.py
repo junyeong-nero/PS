a84355399176e71713b285e466da602a49c39ce3
junyeong-nero <magicwho@unist.ac.kr>
2025-02-10 22:15:07 +0900

    solve 3174

---
+++ leetcode/3174.py
@@ -0,0 +1,10 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        q = deque([])
+        for char in s:
+            if char in "0123456789":
+                q.pop()
+            else:
+                q.append(char)
+
+        return ''.join(q)
