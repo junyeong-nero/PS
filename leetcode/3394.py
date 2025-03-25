e9fce8ad7575efc3f7c7b93bd888454d28770309
junyeong-nero <magicwho@unist.ac.kr>
2025-03-25 20:49:55 +0900

    solve 3394

--- leetcode/3394.py
+++ leetcode/3394.py
@@ -3,17 +3,14 @@ class Solution:
         
         def check_overlap(arr):
             arr = sorted(arr)
-            prev = n + 1
-            res = 0
+            prev = res = -1
             for i in range(len(arr)):
                 if prev <= arr[i][0]:
-                    res += (arr[i][0] - prev + 1)
-                prev = arr[i][1]
-
+                    res += 1
+                prev = max(prev, arr[i][1])
             return res
 
         V = [(rect[0], rect[2]) for rect in rectangles]
         H = [(rect[1], rect[3]) for rect in rectangles]
 
         return check_overlap(V) >= 2 or check_overlap(H) >= 2
-
