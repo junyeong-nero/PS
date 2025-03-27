fa81ebfd84f0b40ca94bde3ef54191b09963e5ed
junyeong-nero <magicwho@unist.ac.kr>
2025-03-27 19:59:46 +0900

    solve 720

---
+++ leetcode/720.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        words.sort()
+        # print(words)
+
+        tree = dict()
+        def add(tree, word):
+            cur = tree
+            check = True
+            for char in word[:-1]:
+                if char not in cur:
+                    check = False
+                    break
+                cur = cur[char]
+            
+            if check:
+                cur[word[-1]] = dict()
+                return len(word)
+            
+            return 0
+
+        res = ""
+        for word in words:
+            size = add(tree, word)
+            if size > len(res):
+                res = word
+        
+        # print(tree)
+        return res
