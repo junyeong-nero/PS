b850987c1b3745549592734c8321db79f883fd5c
junyeong-nero <magicwho@unist.ac.kr>
2025-03-27 19:50:46 +0900

    optimize 2780

--- leetcode/2780.py
+++ leetcode/2780.py
@@ -1,32 +1,13 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
-        m = len(nums)
+        c = Counter(nums)
+        total, d = max((b, a) for a, b in c.items())
 
-        left, right = Counter(), Counter(nums)
-        left_size, right_size = 0, m
-        left_dominant, right_dominant = -1, max(right.keys(), key=lambda x: right[x])
-        # print(right_dominant)
-
-        for i in range(m):
-            elem = nums[i]
-            left[elem] += 1
-            left_size += 1
-            if left[elem] * 2 > left_size:
-                left_dominant = elem
-            elif left[left_dominant] <= left_size:
-                left_dominant = -1
-
-            right[elem] -= 1
-            right_size -= 1
-            if right[elem] * 2 > right_size:
-                right_dominant = elem
-            elif right[right_dominant] <= right_size:
-                right_dominant = -1
-            
-            if left_dominant == right_dominant and left_dominant != -1:
-                # print(left, left_dominant)
-                # print(right, right_dominant)
+        count = 0
+        for i, val in enumerate(nums):
+            if val == d:
+                count += 1
+            if count * 2 > (i + 1) and (total - count) * 2 > len(nums) - i - 1:
                 return i
-        
+
         return -1
-        
