7420bec2e32b8b8fab0ae3ac5d869459cee47d53
junyeong-nero <magicwho@unist.ac.kr>
2025-03-29 11:16:46 +0900

    fix 2818

--- leetcode/2818.py
+++ leetcode/2818.py
@@ -2,6 +2,7 @@ class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
         
         n = len(nums)
+        MOD = 10 ** 9 + 7
 
         def prime_score(num):
             i = 2
@@ -13,24 +14,34 @@ class Solution:
                 i += 1
             return len(primes)
 
-        # print(prime_score(300))
+        scores = [prime_score(x) for x in nums]
+        arr = sorted([(num, idx) for idx, num in enumerate(nums)], reverse=True)
 
-        heap = []
-        for idx, num in enumerate(nums):
-            score = prime_score(num)
-            heappush(heap, (-score, -num, idx))
-
-        MOD = 10 ** 9 + 7
-        # [0, 1, 2, 3] * [3, 4, 5]
+        # print(scores)
+        # print(arr)
 
         res = 1
-        while k > 0:
-            score, num, idx = heappop(heap)
-            count = min(k, (idx + 1) * (n - idx))
-            print(-score, -num, idx, count)
+        i = 0
 
-            res = res * (-num) ** count
-            res = res % MOD
+        while k > 0:
+            if i >= n:
+                break
+            num, index = arr[i]
+
+            x = index
+            y = index
+            while x - 1 >= 0 and scores[x - 1] < scores[index]:
+                x -= 1
+            while y + 1 < n and scores[y + 1] <= scores[index]:
+                y += 1
+
+            # scores[index] = float('inf')
+            count = min(k, (index - x + 1) * (y - index + 1))
+            # print(num, count)
+            res = (res * num ** count) % MOD
+
+            i += 1
             k -= count
-            
+
+
         return res
