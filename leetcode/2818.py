a192faca504b0d7bfa965395576b221a05d1f395
junyeong-nero <magicwho@unist.ac.kr>
2025-03-29 11:30:30 +0900

    fail 2818

--- leetcode/2818.py
+++ leetcode/2818.py
@@ -1,47 +1,57 @@
 class Solution:
+    MOD = 1000000007
+
     def maximumScore(self, nums: List[int], k: int) -> int:
-        
         n = len(nums)
-        MOD = 10 ** 9 + 7
 
-        def prime_score(num):
-            score = 0
-            i = 2
-            while i * i <= num:
-                if num % i == 0:
-                    score += 1
-                    while num % i == 0:
-                        num //= i
-                i += 1
-            if num > 1:
-                score += 1
-            return score
+        upper = max(nums) + 1
 
-        d = {x: prime_score(x) for x in set(nums)} 
-        scores = [d[x] for x in nums]
-        arr = sorted([(num, idx) for idx, num in enumerate(nums)], reverse=True)
+        prime = [True] * upper
+        prime[0] = prime[1] = False
+        primeScore = [0] * upper
+        for i in range(2, upper):
+            if prime[i]:
+                for j in range(i, upper, i):
+                    primeScore[j] += 1
+                    prime[j] = False
 
-        # print(scores)
-        # print(arr)
+        nextGreaterElement = [n] * n
+        s = []
+        for i in range(n - 1, -1, -1):
+            while s and primeScore[nums[i]] >= primeScore[nums[s[-1]]]:
+                s.pop()
+            nextGreaterElement[i] = s[-1] if s else n
+            s.append(i)
+
+        prevGreaterOrEqualElement = [-1] * n
+        s = []
+        for i in range(n):
+            while s and primeScore[nums[i]] > primeScore[nums[s[-1]]]:
+                s.pop()
+            prevGreaterOrEqualElement[i] = s[-1] if s else -1
+            s.append(i)
 
         res = 1
-        i = 0
+        tuples = [[nums[i], i] for i in range(n)]
+        tuples.sort(reverse=True)
+        for num, idx in tuples:
+            operations = min(
+                (idx - prevGreaterOrEqualElement[idx])
+                * (nextGreaterElement[idx] - idx),
+                k,
+            )
+            res = (res * self.pow(num, operations)) % self.MOD
+            k -= operations
+            if k == 0:
+                return res
 
-        while k > 0:
-            if i >= n:
-                break
-            num, index = arr[i]
+        return res
 
-            x = y = index
-            while x - 1 >= 0 and scores[x - 1] < scores[index]:
-                x -= 1
-            while y + 1 < n and scores[y + 1] <= scores[index]:
-                y += 1
-
-            count = min(k, (index - x + 1) * (y - index + 1))
-            res = (res * num ** count) % MOD
-
-            i += 1
-            k -= count
-
+    def pow(self, x: int, n: int) -> int:
+        res = 1
+        while n > 0:
+            if n % 2 == 1:
+                res = (res * x) % self.MOD
+            x = (x * x) % self.MOD
+            n //= 2
         return res
