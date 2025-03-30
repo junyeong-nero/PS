0d16163d9716f5c45dfa061bfbc485128fffbe66
junyeong-nero <magicwho@unist.ac.kr>
2025-03-30 15:25:06 +0900

    optimize 321

--- leetcode/321.py
+++ leetcode/321.py
@@ -1,21 +1,18 @@
 class Solution:
     def maxNumber(self, nums1: List[int], nums2: List[int], k: int) -> List[int]:
-        
         m, n = len(nums1), len(nums2)
-
-        @cache
-        def func(index1, index2, value, count):
-            if count == 0:
-                return value
-
-            res = value
-            for i in range(index1, m):
-                res = max(res, func(i + 1, index2, value * 10 + nums1[i], count - 1))
-
-            for j in range(index2, n):
-                res = max(res, func(index1, j + 1, value * 10 + nums2[j], count - 1))
-
-            return res
         
-        res = func(0, 0, 0, k)
-        return [int(x) for x in str(res)]
+        @lru_cache(None)
+        def func(index1, index2, count):
+            if count == 0:
+                return ""
+            
+            max_res = "-1"
+            for i in range(index1, m):
+                max_res = max(max_res, str(nums1[i]) + func(i + 1, index2, count - 1), key = lambda x: int(x))
+            for j in range(index2, n):
+                max_res = max(max_res, str(nums2[j]) + func(index1, j + 1, count - 1), key = lambda x: int(x))
+            return "" if max_res == "-1" else max_res
+        
+        res = func(0, 0, k)
+        return [int(x) for x in res]
