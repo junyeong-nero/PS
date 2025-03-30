8f6e265455ed41d34fe4741f9f3f7dbe5d29bfda
junyeong-nero <magicwho@unist.ac.kr>
2025-03-30 15:28:31 +0900

    solve 321 by using another greedy approach

--- leetcode/321.py
+++ leetcode/321.py
@@ -1,18 +1,34 @@
 class Solution:
     def maxNumber(self, nums1: List[int], nums2: List[int], k: int) -> List[int]:
-        m, n = len(nums1), len(nums2)
-        
-        @lru_cache(None)
-        def func(index1, index2, count):
-            if count == 0:
-                return ""
-            
-            max_res = "-1"
-            for i in range(index1, m):
-                max_res = max(max_res, str(nums1[i]) + func(i + 1, index2, count - 1), key = lambda x: int(x))
-            for j in range(index2, n):
-                max_res = max(max_res, str(nums2[j]) + func(index1, j + 1, count - 1), key = lambda x: int(x))
-            return "" if max_res == "-1" else max_res
-        
-        res = func(0, 0, k)
-        return [int(x) for x in res]
+        def merge(n1, n2):
+            res = []
+            while n1 or n2:
+                if n1 > n2:
+                    res.append(n1[0])
+                    n1 = n1[1:]
+                else:
+                    res.append(n2[0])
+                    n2 = n2[1:]
+            return res
+
+        def findmax(nums, length):
+            l = []
+            maxpop = len(nums) - length
+            for i in range(len(nums)):
+                while maxpop > 0 and len(l) and nums[i] > l[-1]:
+                    l.pop()
+                    maxpop -= 1
+                l.append(nums[i])
+            return l[:length]
+
+        n1 = len(nums1)
+        n2 = len(nums2)
+        res = [0] * k
+        for i in range(k + 1):
+            j = k - i
+            if i > n1 or j > n2:
+                continue
+            l1 = findmax(nums1, i)
+            l2 = findmax(nums2, j)
+            res = max(res, merge(l1, l2))
+        return res
