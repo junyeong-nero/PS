b9d20577222a6f78f7bec49b631296ca5c9506e7
junyeong-nero <magicwho@unist.ac.kr>
2025-03-30 14:34:56 +0900

    TLE 188

--- leetcode/188.py
+++ leetcode/188.py
@@ -2,24 +2,29 @@ class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         
         n = len(prices)
-        prefix = [(prices[0], 0)]
+
+        benefit = []
         for i in range(1, n):
-
-            temp = prefix[-1]
-            if prices[i] < temp[0]:
-                temp = (prices[i], i)
-
-            prefix.append(temp)
-    
-        benefit = [(prefix[i][1], i, prices[i] - prefix[i][0]) for i in range(n) if prices[i] - prefix[i][0] > 0]
+            for j in range(i):
+                value = prices[i] - prices[j]
+                if value > 0:
+                    benefit.append((j, i, value))
         # print(benefit)
 
-        def func(cur, cost):
+        d = dict()
+
+        @cache
+        def func(cur, cost, count):
+            if (cur, cost, count) in d:
+                return d[(cur, cost, count)]
+            if count == 0:
+                return cost
             res = cost
             for start, end, value in benefit:
                 if start <= cur:
                     continue
-                res = max(res, func(end, cost + value))
+                res = max(res, func(end, cost + value, count - 1))
+            d[(cur, cost, count)] = res
             return res
 
-        return func(-1, 0)
+        return func(-1, 0, k)
