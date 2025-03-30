d16b9a402b1c2a69822cf4b348b844b1653092bd
junyeong-nero <magicwho@unist.ac.kr>
2025-03-30 15:09:25 +0900

    fix 321

--- leetcode/321.py
+++ leetcode/321.py
@@ -1,18 +1,21 @@
 class Solution:
     def maxNumber(self, nums1: List[int], nums2: List[int], k: int) -> List[int]:
         
-        arr = nums1 + nums2
         m, n = len(nums1), len(nums2)
 
-        def func(index, value, count):
+        @cache
+        def func(index1, index2, value, count):
             if count == 0:
                 return value
 
             res = value
-            for i in range(index, m + n):
-                num = arr[i]
-                res = max(res, func(i + 1, value * 10 + num, count - 1))
+            for i in range(index1, m):
+                res = max(res, func(i + 1, index2, value * 10 + nums1[i], count - 1))
+
+            for j in range(index2, n):
+                res = max(res, func(index1, j + 1, value * 10 + nums2[j], count - 1))
+
             return res
         
-        res = func(0, 0, k)
+        res = func(0, 0, 0, k)
         return [int(x) for x in str(res)]
