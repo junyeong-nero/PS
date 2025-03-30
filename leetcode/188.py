93059660f60f4123aac64224da4b19cfdad10da3
junyeong-nero <magicwho@unist.ac.kr>
2025-03-30 14:40:37 +0900

    optimize 188

--- leetcode/188.py
+++ leetcode/188.py
@@ -3,12 +3,12 @@ class Solution:
         
         n = len(prices)
 
-        benefit = []
+        benefit = defaultdict(list)
         for i in range(1, n):
             for j in range(i):
                 value = prices[i] - prices[j]
                 if value > 0:
-                    benefit.append((j, i, value))
+                    benefit[j].append((i, value))
         # print(benefit)
 
         d = dict()
@@ -17,13 +17,16 @@ class Solution:
         def func(cur, cost, count):
             if (cur, cost, count) in d:
                 return d[(cur, cost, count)]
+            
             if count == 0:
                 return cost
             res = cost
-            for start, end, value in benefit:
+            for start, arr in benefit.items():
                 if start <= cur:
                     continue
-                res = max(res, func(end, cost + value, count - 1))
+                for end, value in arr:
+                    res = max(res, func(end, cost + value, count - 1))
+            
             d[(cur, cost, count)] = res
             return res
 
