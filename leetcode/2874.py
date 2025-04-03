61ebb120164e7280527be2d7ef9880bc4a138163
junyeong-nero <magicwho@unist.ac.kr>
2025-04-03 22:14:19 +0900

    solve 2874

---
+++ leetcode/2874.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        # max : nums[i]
+        # min : nums[j]
+        # max : nums[k]
+
+        # min : nums[i]
+        # max : nums[j] > 0 
+        # min : nums[k] < 0
+
+        a = b = nums[0]
+        c = d = 0
+        res = 0
+
+        for num in nums[1:]:
+            
+            res = max(res, c * num)
+            res = max(res, d * num)
+
+            c = max(c, a - num)
+            d = min(d, b - num)
+
+            a = max(a, num)
+            b = min(b, num)
+
+        return res
