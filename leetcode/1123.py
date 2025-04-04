d4b1c4e28ca192af46bd8873fa37dfe656f22400
junyeong-nero <magicwho@unist.ac.kr>
2025-04-04 23:39:05 +0900

    optimized 1123

--- leetcode/1123.py
+++ leetcode/1123.py
@@ -7,45 +7,20 @@
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
 
-        d = []
-        cur_depth = -1
-        nodes = {}
+        d = dict()
 
-        def dfs(cur, his, depth):
-            nonlocal cur_depth, d
-            nodes[cur.val] = cur
+        def func(cur, depth):
+            nonlocal d
+            if not cur:
+                return depth
             
-            if not cur.left and not cur.right:
-                # leaf node
-                # print(cur.val, his)
-                if cur_depth < depth:
-                    cur_depth = depth
-                    d = his[:] + [cur.val]
-                elif cur_depth == depth:
-                    temp = []
-                    p = set(his)
-                    for i in range(len(d) - 1, -1, -1):
-                        value = d[i]
-                        if value in p:
-                            temp.append(value)
-                    d = temp[::-1]
-                return
+            right = func(cur.left, depth + 1)
+            left = func(cur.right, depth + 1)
+            if right == left:
+                d[right] = cur
+            return max(right, left)
 
-            his.append(cur.val)
-            if cur.left:
-                dfs(cur.left, his, depth + 1)
-            
-            if cur.right:
-                dfs(cur.right, his, depth + 1)
-            his.pop()
-
-        dfs(root, [], 0)
-        
-        # print(d)
-        # print(cur_depth)
-        # print(nodes)
-
-        return nodes[d[-1]]
-
+        max_depth = func(root, 0)
+        return d[max_depth]
 
 
