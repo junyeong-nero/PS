3cbb52a1a1c0fdf426d5d91bb24cace5745a5862
junyeong-nero <magicwho@unist.ac.kr>
2025-04-05 19:45:58 +0900

    solve 1863 with bitmask

--- leetcode/1863.py
+++ leetcode/1863.py
@@ -1,16 +1,6 @@
-
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-
-        n = len(nums)
-
-        @cache
-        def func(cur, value):
-            if cur >= n:
-                return value
-            res = value
-            for i in range(cur, n):
-                res += func(i + 1, value ^ nums[i])
-            return res
-            
-        return func(0, 0)
+        ans = 0
+        for i in nums:
+            ans |= i
+        return ans << (len(nums) - 1)
