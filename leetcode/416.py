12cc5c69c65c8b1a08f97365d0fa478d9e8cae1b
junyeong-nero <magicwho@unist.ac.kr>
2025-04-07 21:19:45 +0900

    wrong approach

--- leetcode/416.py
+++ leetcode/416.py
@@ -3,24 +3,13 @@ class Solution:
         total = sum(nums)
         if total % 2 == 1:
             return False
-            
+
         n = len(nums)
+        nums.sort()
 
-        def func(target):
-            if target == 0:
-                return True
-            if target < 0:
-                return False
+        prefix = [0]
+        for num in nums:
+            prefix.append(prefix[-1] + num)
 
-            res = False
-            for i in range(n):
-                if nums[i] == 0:
-                    continue
-                temp, nums[i] = nums[i], 0
-                if func(target - temp):
-                    return True
-                nums[i] = temp
-
-            return False
-        
-        return func(total // 2)
+        idx = bisect_left(prefix, total // 2)
+        return idx < n and (prefix[idx] == total // 2)
