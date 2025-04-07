c8250a49a6b534949fd9a575fccc7fb2de0c9f3d
junyeong-nero <magicwho@unist.ac.kr>
2025-04-07 21:24:29 +0900

    solve 416 with DFS

--- leetcode/416.py
+++ leetcode/416.py
@@ -1,15 +1,25 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
         total = sum(nums)
         if total % 2 == 1:
             return False
 
-        n = len(nums)
-        nums.sort()
+        @cache
+        def func(index, target):
+            if target == 0:
+                return True
+            if target < 0:
+                return False
 
-        prefix = [0]
-        for num in nums:
-            prefix.append(prefix[-1] + num)
+            for i in range(index, n):
+                if nums[i] == 0:
+                    continue
+                temp, nums[i] = nums[i], 0
+                if func(index + 1, target - temp):
+                    return True
+                nums[i] = temp
 
-        idx = bisect_left(prefix, total // 2)
-        return idx < n and (prefix[idx] == total // 2)
+            return False
+        
+        return func(0, total // 2)
