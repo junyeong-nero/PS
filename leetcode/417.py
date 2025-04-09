61bbfceb075a8e2b5e52d1dba0abc058e703cf4e
junyeong-nero <magicwho@unist.ac.kr>
2025-04-09 20:20:55 +0900

    solve 417

--- leetcode/417.py
+++ leetcode/417.py
@@ -4,30 +4,46 @@ class Solution:
         m, n = len(heights), len(heights[0])
         po = [[False] * n for _ in range(m)]
         ao = [[False] * n for _ in range(m)]
+
+        po_start = []
+        ao_start = []
         for i in range(m):
+            po_start.append((i, 0))
+            ao_start.append((i, n - 1))
             po[i][0] = True
             ao[i][-1] = True
 
+
         for j in range(n):
+            po_start.append((0, j))
+            ao_start.append((m - 1, j))
             po[0][j] = True
-            ao[-1][j] = True
+            ao[m - 1][j] = True
 
-        for x in range(1, m):
-            for y in range(1, n):
-                if heights[x][y] >= heights[x - 1][y]:
-                    po[x][y] |=  po[x - 1][y]
-                if heights[x][y] >= heights[x][y - 1]:
-                    po[x][y] |=  po[x][y - 1]
         
-        for x in range(m - 2, -1, -1):
-            for y in range(n - 2, -1, -1):
-                if heights[x][y] >= heights[x + 1][y]:
-                    ao[x][y] |=  ao[x + 1][y]
-                if heights[x][y] >= heights[x][y + 1]:
-                    ao[x][y] |=  ao[x][y + 1]
+        def func(start_points, target):
+            q = deque(start_points)
+            while q:
+                x, y = q.popleft()
 
-        print(po)
-        print(ao)
+                dirs = [1, 0, -1, 0, 1]
+                for i in range(4):
+                    nx, ny = x + dirs[i], y + dirs[i + 1]
+                    if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                        continue
+                    if target[nx][ny]:
+                        continue
+                    if heights[nx][ny] < heights[x][y]:
+                        continue
+
+                    target[nx][ny] = True
+                    q.append((nx, ny))
+
+        func(po_start, po)
+        func(ao_start, ao)
+        
+        # print(po)
+        # print(ao)
 
         res = []
         for x in range(m):
