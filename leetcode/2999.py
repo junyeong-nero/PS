aa10c6b74834dd7acbe1b35c430582a635183e19
junyeong-nero <magicwho@unist.ac.kr>
2025-04-10 21:01:09 +0900

    solve 2999

--- leetcode/2999.py
+++ leetcode/2999.py
@@ -1,38 +1,14 @@
 class Solution:
-    def numberOfPowerfulInt(self, start: int, finish: int, limit: int, s: str) -> int:
-        n = len(s)
-        a, b = str(start), str(finish)
-
-        def func(x):
-            temp = int(x)
-            return temp
-            # return temp if temp <= limit else limit
-
-        start_t = [0]
-        if len(a) > 2:
-            start_t = list(map(func, a[:-n]))
-        finish_t = list(map(func, b[:-n]))
-
-        def order(arr, base):
-            res = 0
-            temp = 1
-            carry = 0
-            for num in arr[::-1]:
-                if carry + num >= base:
-                    carry = 1
-                    num = 0
-                else:
-                    carry = 0
-
-                res += temp * num
-                temp *= base
-            if carry == 1:
-                res += temp
-            return res
-
-        print(start_t, finish_t)
-        a = order(finish_t, limit + 1)
-        b = order(start_t, limit + 1)
-        print(a, b)
-
-        return a - b
+    def numberOfPowerfulInt(self, start: int, finish: int, l: int, s: str) -> int:
+        cnt = [0] * 16
+        def count(n : str, s: str) -> int:
+            res, i, sz = cnt[len(n) - 1], 0, len(n) - len(s)
+            while True:
+                res += n[i:] >= s if i == sz else cnt[len(n) - i - 1] * (min(l, int(n[i]) - 1) + (i > 0))
+                i += 1
+                if i > sz or int(n[i - 1]) > l:
+                    break
+            return res            
+        for i in range(len(s), 16):
+            cnt[i] = 1 if i == len(s) else cnt[i - 1] * (l + 1)
+        return count(str(finish), s) - count(str(start - 1), s)
