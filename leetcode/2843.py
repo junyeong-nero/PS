6ec6ec6e5569897f7a6f07fb68fbe460cc5ad43c
junyeong-nero <magicwho@unist.ac.kr>
2025-04-11 21:37:39 +0900

    sokve 2843

---
+++ leetcode/2843.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        res = 0
+        for num in range(low, high + 1):
+            temp = list(map(int, str(num)))
+            # print(temp)
+            n = len(temp)
+            if n % 2 == 1:
+                continue
+            if sum(temp[:n // 2]) == sum(temp[n // 2:]):
+                res += 1
+        
+        return res
