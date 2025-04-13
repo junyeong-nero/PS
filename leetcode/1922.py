a1c1f8397d878a5312e86bb2a7eb587cbd2092ac
junyeong-nero <magicwho@unist.ac.kr>
2025-04-13 22:44:17 +0900

    solve 1922

--- leetcode/1922.py
+++ leetcode/1922.py
@@ -1,10 +1,15 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         MOD = 10 ** 9 + 7
-
-        # 5 (0, 2, 4, 6, 8)
-        # 4 (2, 3, 5, 7)
+        
         odd = n // 2
         even = n - odd 
 
-        return (5 ** even) * (4 ** odd) % MOD
+        # a = (5 ** even) * (4 ** odd)
+        # res = 20 ** odd % MOD
+        
+        # use pow function rather than ** operator
+        res = pow(20, odd, MOD)
+        if odd != even:
+            res = (5 * res) % MOD
+        return res
