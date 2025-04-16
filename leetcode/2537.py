dacbe8f21af4d0b17e8aa2087bc856cf9963eb78
junyeong-nero <magicwho@unist.ac.kr>
2025-04-16 23:51:23 +0900

    solve 2537

--- leetcode/2537.py
+++ leetcode/2537.py
@@ -1,22 +1,16 @@
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
-        counter = Counter()
         n = len(nums)
-        i = j = 0
-        res = 0
-        while j < n and i <= j:
-            counter[nums[j]] += 1
-            temp = sum([value * (value - 1) // 2 for value in counter.values()])
-            print(i, j, temp)
-            
-            if temp < k:
-                j += 1
-            else:
-                # res += (i + 1) * (n - j)
-                res += (n - j)
-                counter[nums[i]] -= 1
-                i += 1
-                if j < n - 1:
-                    j += 1
-        
-        return res
+        same, right = 0, -1
+        cnt = Counter()
+        ans = 0
+        for left in range(n):
+            while same < k and right + 1 < n:
+                right += 1
+                same += cnt[nums[right]]
+                cnt[nums[right]] += 1
+            if same >= k:
+                ans += n - right
+            cnt[nums[left]] -= 1
+            same -= cnt[nums[left]]
+        return ans
