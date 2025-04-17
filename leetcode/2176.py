6d971f05eac5c3cc0dd4ede2512b9d9631d12360
junyeong-nero <magicwho@unist.ac.kr>
2025-04-17 23:16:25 +0900

    solve 2176.py

---
+++ leetcode/2176.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        d = defaultdict(list)
+        for idx, num in enumerate(nums):
+            d[num].append(idx)
+        
+        res = 0
+        for key, value in d.items():
+            n = len(value)
+            for i in range(n):
+                for j in range(i + 1, n):
+                    if (value[i] * value[j]) % k == 0:
+                        res += 1
+
+        return res
+
