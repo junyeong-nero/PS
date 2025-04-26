815fc088d42c7137068b1d88ebc5917e76c98796
junyeong-nero <magicwho@unist.ac.kr>
2025-04-26 21:36:52 +0900

    solve 3438

---
+++ leetcode/3438.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findValidPair(self, s: str) -> str:
+        counter = Counter(s)
+        for i in range(len(s) - 1):
+            a, b = s[i], s[i + 1]
+            if a == b:
+                continue
+            if a == str(counter[a]) and b == str(counter[b]):
+                return a + b
+        
+        return ""
