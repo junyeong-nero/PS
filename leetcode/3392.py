f4bcf27b300a62a19aede72419bf486aa32cf730
junyeong-nero <magicwho@unist.ac.kr>
2025-04-27 13:36:24 +0900

    solve 3392

---
+++ leetcode/3392.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        q = deque(nums[:3])
+        n = len(nums)
+        res = 0
+        for i in range(3, n):
+            # print(q)
+            if q[0] + q[2] == q[1] / 2:
+                res += 1
+            q.popleft()
+            q.append(nums[i])
+        
+        if q[0] + q[2] == q[1] / 2:
+            res += 1
+        
+        return res
+            
