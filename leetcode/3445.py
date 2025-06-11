3128f39a29fd81b98fc3284608ac1e70e3d548a9
junyeong-nero <magicwho@unist.ac.kr>
2025-06-11 23:37:23 +0900

    fail 3445

--- leetcode/3445.py
+++ leetcode/3445.py
@@ -1,42 +1,35 @@
 class Solution:
     def maxDifference(self, s: str, k: int) -> int:
-        
+        def getStatus(cnt_a: int, cnt_b: int) -> int:
+            return ((cnt_a & 1) << 1) | (cnt_b & 1)
+
         n = len(s)
-
-        def diff(info):
-            arr = list(info.values())
-            if sum(arr) < k:
-                return -float('inf')
-
-            _min, _max = float('inf'), -1
-            for num in arr:
-                if num <= 0:
+        ans = float("-inf")
+        for a in ["0", "1", "2", "3", "4"]:
+            for b in ["0", "1", "2", "3", "4"]:
+                if a == b:
                     continue
-                if num % 2 == 1:
-                    _max = max(_max, num)
-                else:
-                    _min = min(_min, num)
 
-            if _min == float('inf') or _max == -1:
-                return -float('inf')
-            return _max - _min 
-        
-        def dfs(index, info):
+                best = [float("inf")] * 4
+                cnt_a = cnt_b = 0
+                prev_a = prev_b = 0
+                left = -1
+                for right in range(n):
+                    cnt_a += s[right] == a
+                    cnt_b += s[right] == b
+                    while right - left >= k and cnt_b - prev_b >= 2:
+                        left_status = getStatus(prev_a, prev_b)
+                        best[left_status] = min(
+                            best[left_status], prev_a - prev_b
+                        )
+                        left += 1
+                        prev_a += s[left] == a
+                        prev_b += s[left] == b
 
-            temp = diff(info)
-            if index >= len(s):
-                return temp
-                
-            temp = max(temp, dfs(index + 1, info))
-            info[s[index]] += 1
-            temp = max(temp, dfs(index + 1, info))
-            info[s[index]] -= 1
+                    right_status = getStatus(cnt_a, cnt_b)
+                    if best[right_status ^ 0b10] != float("inf"):
+                        ans = max(
+                            ans, cnt_a - cnt_b - best[right_status ^ 0b10]
+                        )
 
-            return temp
-            
-    
-        res = dfs(0, {s:0 for s in "01234"})
-        return res
-
-        
-
+        return ans
