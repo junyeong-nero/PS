87c011b3aa125ef1f67416698dde006699a1d143
junyeong-nero <magicwho@unist.ac.kr>
2025-06-14 21:20:59 +0900

    solve 447

--- leetcode/447.py
+++ leetcode/447.py
@@ -1,29 +1,30 @@
-
 class Solution:
     def numberOfBoomerangs(self, points: List[List[int]]) -> int:
-
+        n = len(points)
         res = 0
+
         def dist(point1, point2):
             return (point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2
 
         # dist(points[i], points[j]) = dist(points[i], points[k])
-        dist_map = dict()
-        for i, point1 in enumerate(points):
-            for j, point2 in enumerate(points):
-                dist_map[(i, j)] = dist(point1, point2)
+        # dist_map = dict()
+        dist_map = defaultdict(list)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                point1, point2 = points[i], points[j]
+                d = dist(point1, point2)
+
+                dist_map[d].append((i, j))
+                
         print(dist_map)
 
-        n = len(points)
-        for i in range(n):
-            for j in range(n):
-                for k in range(n):
-                    if i == j or i == k or j == k:
-                        continue
-
-                    a = dist_map[(i, j)]
-                    b = dist_map[(i, k)]
-
-                    if a == b:
-                        res += 1
+        for key, rows in dist_map.items():
+            m = len(rows)
+            for i in range(m):
+                for j in range(i + 1, m):
+                    a, b = rows[i], rows[j]
+                    if len(set(a) | set(b)) == 3:
+                        res += 2
 
         return res
