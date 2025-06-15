188cb9a0b3deda1f23f77c2e49df7958d1ae0f3a
junyeong-nero <magicwho@unist.ac.kr>
2025-06-15 21:41:48 +0900

    solve 215

---
+++ leetcode/215.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+        arr = []
+        for num in nums:
+            heappush(arr, -num)
+
+        res = 0
+        for _ in range(k):
+            res = -heappop(arr)
+        return res
