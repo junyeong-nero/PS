71e046468ea9bbca48d59bfd3774e3affa753435
junyeong-nero <magicwho@unist.ac.kr>
2025-06-17 23:38:03 +0900

    fail 3405 with DFS

--- leetcode/3405.py
+++ leetcode/3405.py
@@ -1,30 +1,19 @@
 class Solution:
     def countGoodArrays(self, n: int, m: int, k: int) -> int:
-        # k = 5
-        # [a, a]  5
-        # [a, a, a]
-
         MOD = 10 ** 9 + 7
 
-        if k == 0:
-            return m ** n % MOD
-
-        dp = [[0] * (k + 1) for _ in range(k + 1)]
-        dp[1][1] = 1
-        for i in range(1, k + 1):
-            for j in range(1, i):
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - j][j]
-        print(dp)
-
-        def P(a, b):
-            if b == 1 or a == b:
+        def dfs(cur, size, left):
+            if left < 0:
+                return 0
+            if size == n:
                 return 1
-            # 1이 있는 경우 -> 1만 추가하면 되니: P(a - 1, b - 1)
-            # 1이 없는 경우 -> 적어도 b개의 항목이 2가 되어야 함: P(a - b, b)
-            return P(a - 1, b - 1) + P(a - b, b)
+                
+            res = 0
+            for num in range(1, m + 1):
+                diff = 1 if num == cur else 0
+                res += dfs(num, size + 1, left - diff)
 
-        def P_all(a):
-            arr = [P(a, i) for i in range(1, a + 1)]
+            return res % MOD
         
-        return 0
+        return dfs(0, 0, k)
                 
