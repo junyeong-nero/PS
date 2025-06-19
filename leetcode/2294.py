9462b70c7bb945c695af6c09ab11d47dbefa20f8
junyeong-nero <magicwho@unist.ac.kr>
2025-06-19 23:15:13 +0900

    solve 2294 with sort + greedy

--- leetcode/2294.py
+++ leetcode/2294.py
@@ -1,38 +1,12 @@
 class Solution:
-    def partitionArray(self, nums: List[int], k: int) -> int:
-        
-        n = len(nums)
-        res = 0 # [1, n]
-
-        def func(index, his):
-            arr = [nums[i] for i in his]
-            if his and (max(arr) - min(arr)) > k:
-                return []
-            if index >= n:
-                return his
-
-            res = his[:]
-            temp = func(index + 1, his)
-            if len(temp) > len(res):
-                res = temp
-
-            # select
-            if nums[index] != -1:
-                temp = func(index + 1, his + [index])
-                if len(temp) > len(res):
-                    res = temp
-
-            return res
-
-
-        total = 0
-        while total < n:
-            used = func(0, [])
-            # print(used)
-            # print(nums)
-            for i in used:
-                nums[i] = -1
-            total += len(used)
-            res += 1
-
+    def partitionArray(self, A, k):
+        A.sort()
+        res = 1
+        mn = mx = A[0]
+        for a in A:
+            mn = min(mn, a)
+            mx = max(mx, a)
+            if mx - mn > k:
+                res += 1
+                mn = mx = a
         return res
