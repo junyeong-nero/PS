c1a52c8ad2ab41e35b60971cf6aa2b2729ca018e
junyeong-nero <magicwho@unist.ac.kr>
2025-06-21 23:11:38 +0900

    solve 3443

--- leetcode/3443.py
+++ leetcode/3443.py
@@ -1,26 +1,13 @@
 class Solution:
     def maxDistance(self, s: str, k: int) -> int:
-        counter = Counter(s)
-        n = len(s)
-
-        dirs = [1, 0, -1, 0, 1]
-        map_ = {
-            "N": 0,
-            "S": 2,
-            "W": 1,
-            "E": 3,
-        }
-
-        res = k
-
-        x, y = 0, 0
-        for i, c in enumerate(s):
-            dx, dy = dirs[map_[c]], dirs[map_[c] + 1]
-            x, y = x + dx, y + dy
-            flag = (x > 0, y > 0)
-            temp = abs(x) + abs(y) + min(n - i - 1, k)
-            res = max(res, temp)
-
-        # print(res)
+        res = 0
+        for diag in [{"N", "E"}, {"N", "W"}, {"S", "E"}, {"S", "W"}]:
+            kk, dist = k, 0
+            for ch in s:
+                if ch in diag or kk:
+                    dist += 1
+                    kk -= ch not in diag
+                else:
+                    dist -= 1
+                res = max(res, dist)
         return res
-            
