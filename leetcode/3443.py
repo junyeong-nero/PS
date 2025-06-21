2b892887054d8048231b5f05ff26d78b38a9989a
junyeong-nero <magicwho@unist.ac.kr>
2025-06-21 22:52:38 +0900

    TLE 3443

--- leetcode/3443.py
+++ leetcode/3443.py
@@ -1,12 +1,30 @@
 class Solution:
     def maxDistance(self, s: str, k: int) -> int:
         counter = Counter(s)
-        n, s, e, w = counter["N"], counter["S"], counter["E"], counter["W"]
-        if n < s:
-            n, s = s, n
-        if e < w:
-            e, w = w, e
-        
-        # n > s
-        # e > w
-        return n + e + min(s + w, k) * 2
+        n = len(s)
+
+        map_ = {
+            "N": 0,
+            "S": 2,
+            "W": 1,
+            "E": 3,
+        }
+
+        def dfs(x, y, index, left):
+            res = abs(x) + abs(y)
+            if index >= n:
+                return res
+            dirs = [1, 0, -1, 0, 1]
+            i = map_[s[index]]
+            dx, dy = dirs[i], dirs[i + 1]
+            res = max(res, dfs(x + dx, y + dy, index + 1, left))
+            if left > 0:
+                for i in range(4):
+                    dx, dy = dirs[i], dirs[i + 1]
+                    res = max(res, dfs(x + dx, y + dy, index + 1, left - 1))
+            
+            return res
+
+        res = dfs(0, 0, 0, k)
+        return res
+            
