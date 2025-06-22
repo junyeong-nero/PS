9a2961a5ca7bf779b3ce16f0b2ab0a27ec4cc0ba
junyeong-nero <magicwho@unist.ac.kr>
2025-06-22 20:41:39 +0900

    solve 6

---
+++ 6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+
+        d = [[] for _ in range(numRows)]
+
+        direction = -1
+        index = 0
+        for i, c in enumerate(s):
+            d[index].append(c)
+            if index % numRows == 0 or index % numRows == numRows - 1:
+                direction *= -1
+            index += direction
+        
+        # print(d)
+        return "".join(["".join(row) for row in d])
+            
+
