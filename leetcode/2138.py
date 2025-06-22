b0d76e7fe13f2dd31548ec4d07bbdda99cf202dc
junyeong-nero <magicwho@unist.ac.kr>
2025-06-22 20:26:02 +0900

    solve 2138

---
+++ leetcode/2138.py
@@ -0,0 +1,10 @@
+class Solution:
+    def divideString(self, s: str, k: int, fill: str) -> List[str]:
+        n = len(s)
+
+        res = []
+        for i in range(0, n, k):
+            res.append(s[i:i + k])
+
+        res[-1] = res[-1] + fill * (k - len(res[-1]))
+        return res
