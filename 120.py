0bac0751d323d64ef3061ad7dc1cd744cab5f414
junyeong-nero <magicwho@unist.ac.kr>
2025-06-22 21:00:34 +0900

    solve 120 with 1D array DP

--- 120.py
+++ 120.py
@@ -1,12 +1,13 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         m = len(triangle)
-        
-        dp = [[float('inf')] * m for _ in range(m)]
-        dp[0][0] = triangle[0][0]
+        dp = [float('inf')] * m
+        dp[0] = triangle[0][0]
         for i in range(1, m):
+            new_dp = [float('inf')] * m
             for j in range(i + 1):
-                dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1] if j - 1 >= 0 else float('inf')) + triangle[i][j]
+                new_dp[j] = min(dp[j], dp[j - 1] if j - 1 >= 0 else float('inf')) + triangle[i][j]
+            dp = new_dp
 
         # print(dp)
-        return min(dp[-1])
+        return min(dp)
