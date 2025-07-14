7dc55c89b7dfcf4e146b11c16a87034e374e2a99
junyeong-nero <magicwho@unist.ac.kr>
2025-07-14 11:07:31 +0900

    solve 1290

---
+++ leetcode/1290.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        cur = head
+        num = 0
+        while cur:
+            num = num * 2 + cur.val
+            cur = cur.next
+        
+        return num
