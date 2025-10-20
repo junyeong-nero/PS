d6fd03b1419da52d9339286ec987e417659b4c2c
junyeong-nero <magicwho@unist.ac.kr>
2025-10-20 10:28:57 +0900

    [TLE] 3186 with DP

--- leetcode/3186.py
+++ leetcode/3186.py
@@ -1,18 +1,28 @@
+from collections import *
+from typing import *
+from bisect import *
+
+
 class Solution:
     def maximumTotalDamage(self, power: List[int]) -> int:
         counter = Counter(power)
         keys = sorted(list(counter.keys()))
         n = len(keys)
 
-        def dfs(index=0, lower_bound=-1):
-            temp = 0
-            for i in range(index, n):
-                key = keys[i]
-                if key <= lower_bound:
-                    continue
-                temp = max(temp, key * counter[key] + dfs(i + 1, keys[i] + 2))
-            return temp
+        # def dfs(index=0, lower_bound=-1):
+        #     temp = 0
+        #     for i in range(index, n):
+        #         key = keys[i]
+        #         if key <= lower_bound:
+        #             continue
+        #         temp = max(temp, key * counter[key] + dfs(i + 1, keys[i] + 2))
+        #     return temp
 
-        result = dfs()
-        print(result)
-        return result
+        dp = [0] * n
+        for i in range(n):
+            key = keys[i]
+            bound = bisect_left(keys, key - 2)
+            dp[i] = max(dp[i], key * counter[key] + max(dp[:bound] + [0]))
+
+        # print(dp)
+        return max(dp)
