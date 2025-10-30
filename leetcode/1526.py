416e46a57b432593fdff3726606ff009c326597d
junyeong-nero <magicwho@unist.ac.kr>
2025-10-30 21:41:32 +0900

    [fail] memory optimization

--- leetcode/1526.py
+++ leetcode/1526.py
@@ -4,40 +4,26 @@ class Solution:
         # print(levels)
 
         n = len(levels)
-        maps = {level: index for index, level in enumerate(levels)}
-        maps_r = {index: level for index, level in enumerate(levels)}
-        # print(maps)
+        weights = []
+        for index, level in enumerate(levels):
+            weights.append(levels[index] - levels[index - 1])
+            maps[level] = index
 
         target = [maps[elem] for elem in target]
-        # print(target)
-
         boundary = defaultdict(list)
         for i in range(len(target)):
             for j in range(n):
                 if target[i] >= j:
-                    boundary[j].append(i)
+                    if j < len(boundary) and boundary[j][-1][1] == i:
+                        boundary[j][-1][1] = i + 1
+                    else:
+                        boundary[j].append([i, i + 1])
+        print(boundary)
 
-        def continuous_list(arr):
-            i = 0
-            m = len(arr)
-            temp = []
-            while i < m:
-                j = i
-                while j + 1 < m and arr[j] + 1 == arr[j + 1]:
-                    j += 1
-                temp.append([i, j])
-                i = j + 1
-            return temp
-
-        for key, value in boundary.items():
-            boundary[key] = continuous_list(value)
-        # print(boundary)
-
-        weights = [maps_r.get(i, 0) - maps_r.get(i - 1, 0) for i in range(n)]
         # print(weights)
 
         result = 0
         for i in range(n):
             result += len(boundary[i]) * weights[i]
-            
+
         return result
