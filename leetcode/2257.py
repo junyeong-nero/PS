e8ea8ca5fc82437806be758aac5847d5d4be265d
junyeong-nero <magicwho@unist.ac.kr>
2025-11-02 15:40:04 +0900

    [solve] 2257

--- leetcode/2257.py
+++ leetcode/2257.py
@@ -1,31 +1,53 @@
 class Solution:
+    UNGUARDED = 0
+    GUARDED = 1
+    GUARD = 2
+    WALL = 3
+
+    # Depth-First Search to mark guarded cells
+    def _recurse(
+        self, row: int, col: int, grid: List[List[int]], direction: str
+    ) -> None:
+        if (
+            row < 0
+            or row >= len(grid)
+            or col < 0
+            or col >= len(grid[0])
+            or grid[row][col] == self.GUARD
+            or grid[row][col] == self.WALL
+        ):
+            return
+
+        grid[row][col] = self.GUARDED  # Mark cell as guarded
+        if direction == "U":
+            self._recurse(row - 1, col, grid, "U")  # Up
+        if direction == "D":
+            self._recurse(row + 1, col, grid, "D")  # Down
+        if direction == "L":
+            self._recurse(row, col - 1, grid, "L")  # Left
+        if direction == "R":
+            self._recurse(row, col + 1, grid, "R")  # Right
+
     def countUnguarded(
         self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]
     ) -> int:
+        grid = [[self.UNGUARDED] * n for _ in range(m)]
 
-        walls = [tuple(wall) for wall in walls]
-        guards = [tuple(guard) for guard in guards]
-
-        def check_guard(guard):
-            x, y = guard
-            pos = [1, 0, -1, 0, 1]
-            blocks = set()
-            for i in range(4):
-                dx, dy = pos[i], pos[i + 1]
-                tx, ty = x + dx, y + dy
-
-                while 0 <= tx < m and 0 <= ty < n:
-                    if (tx, ty) in walls:
-                        break
-                    if (tx, ty) in guards:
-                        break
-                    blocks.add((tx, ty))
-                    tx, ty = tx + dx, ty + dy
-
-            return blocks
-
-        res = set(walls) | set(guards)
+        # Mark guards' positions
         for guard in guards:
-            res |= check_guard(guard)
+            grid[guard[0]][guard[1]] = self.GUARD
 
-        return m * n - len(res)
+        # Mark walls' positions
+        for wall in walls:
+            grid[wall[0]][wall[1]] = self.WALL
+
+        # Mark cells as guarded by traversing from each guard
+        for guard in guards:
+            self._recurse(guard[0] - 1, guard[1], grid, "U")  # Up
+            self._recurse(guard[0] + 1, guard[1], grid, "D")  # Down
+            self._recurse(guard[0], guard[1] - 1, grid, "L")  # Left
+            self._recurse(guard[0], guard[1] + 1, grid, "R")  # Right
+
+        # Count unguarded cells
+        count = sum(row.count(self.UNGUARDED) for row in grid)
+        return count
