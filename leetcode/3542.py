91d5615c5d9b44fe17bd1dd1f22b1045ce1ccbfb
junyeong-nero <magicwho@unist.ac.kr>
2025-11-10 10:34:59 +0900

    solve 3542 with stacks

--- leetcode/3542.py
+++ leetcode/3542.py
@@ -1,31 +1,13 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        n = len(nums)
-        set_nums = set(nums)
-        if not nums or n == 0 or set_nums == {0}:
-            return 0
-        if len(set_nums) == 1:
-            return 1
-
-        min_value = min(nums)
+        stack = []
         res = 0
-        if min_value > 0:
-            res += 1
-            for i in range(n):
-                if nums[i] == min_value:
-                    nums[i] = 0
-
-        boundary = [[n + 1, -1]]
-        for i in range(n):
-            if nums[i] == 0:
-                boundary.append([n + 1, -1])
+        for num in nums:
+            while stack and stack[-1] > num:
+                stack.pop()
+            if num == 0:
                 continue
-
-            boundary[-1][0] = min(boundary[-1][0], i)
-            boundary[-1][1] = max(boundary[-1][1], i)
-
-        for bound in boundary:
-            subset = nums[bound[0] : bound[1] + 1]
-            res += self.minOperations(subset)
-
+            if not stack or stack[-1] < num:
+                res += 1
+                stack.append(num)
         return res
