32ab7e18e99fd029625efc21b13c51b8291757a1
junyeong-nero <magicwho@unist.ac.kr>
2025-11-15 12:58:48 +0900

    TLE 3234 with prefix sum

--- leetcode/3234.py
+++ leetcode/3234.py
@@ -1,20 +1,20 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
 
-        n = len(s)
         prefix = [0]
         for char in s:
             prefix.append(prefix[-1] + int(char))
 
         # prefix[i] = sum(s[:i])
-        print(prefix)
+        # print(prefix)
 
         res = 0
-        for i in range(1, n + 1):
-            for j in range(i + 1, n + 1):
+        n = len(prefix)
+        for i in range(n):
+            for j in range(i + 1, n):
                 num_ones = prefix[j] - prefix[i]
                 num_zeros = j - i - num_ones
-                print(s[i:j], num_ones, num_zeros)
+                # print(s[i:j], num_zeros, num_ones)
                 if num_zeros**2 <= num_ones:
                     res += 1
 
