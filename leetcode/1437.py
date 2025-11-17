693e0d7305ee3a73a369173b9c044b43608abc74
junyeong-nero <magicwho@unist.ac.kr>
2025-11-17 10:06:45 +0900

    [refactor] 1437

--- leetcode/1437.py
+++ leetcode/1437.py
@@ -1,11 +1,16 @@
 class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
-        arr = []
+        prev = None
         n = len(nums)
         for index, value in enumerate(nums):
-            if value == 1:
-                arr.append(index)
+            if value == 0:
+                continue
+            if prev:
+                cur = index - prev - 1
+                print(cur)
+                if cur < k:
+                    return False
 
-        arr = [arr[i + 1] - arr[i] - 1 for i in range(len(arr) - 1)]
-        # print(arr)
-        return all([elem >= k for elem in arr])
+            prev = index
+
+        return True
