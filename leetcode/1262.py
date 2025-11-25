aee5f295dec0a73855c7b0e2a30961d93ecfb593
junyeong-nero <magicwho@unist.ac.kr>
2025-11-25 13:32:01 +0900

    [solve] 1262 with DP

--- leetcode/1262.py
+++ leetcode/1262.py
@@ -1,24 +1,9 @@
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        n = len(nums)
-        d = defaultdict(list)
-        nums = sorted(nums, reverse=True)
+        f = [0, -float("inf"), -float("inf")]
         for num in nums:
-            d[num % 3].append(num)
-
-        tot = sum(nums)
-        remove = float("inf")
-        if tot % 3 == 0:
-            remove = 0
-        elif tot % 3 == 1:
-            if len(d[1]) >= 1:
-                remove = min(remove, d[1][-1])
-            if len(d[2]) >= 2:
-                remove = min(remove, d[2][-2] + d[2][-1])
-        else:
-            if len(d[1]) >= 2:
-                remove = min(remove, d[1][-2] + d[1][-1])
-            if len(d[2]) >= 1:
-                remove = min(remove, d[2][-1])
-
-        return tot - remove
+            g = f[:]
+            for i in range(3):
+                g[(i + num % 3) % 3] = max(g[(i + num % 3) % 3], f[i] + num)
+            f = g
+        return f[0]
