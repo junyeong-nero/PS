9357f5f3e2849db14b06a538f4a7fd0b9fe51426
junyeong-nero <magicwho@unist.ac.kr>
2025-11-25 13:30:47 +0900

    [solve] 1262

--- leetcode/1262.py
+++ leetcode/1262.py
@@ -1,10 +1,24 @@
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
         n = len(nums)
-        res = 0
-        for i in range(n, 0, -1):
-            arr = list(combinations(nums, i))
-            temp = max([0] + [sum(elem) for elem in arr if sum(elem) % 3 == 0])
-            res = max(res, temp)
+        d = defaultdict(list)
+        nums = sorted(nums, reverse=True)
+        for num in nums:
+            d[num % 3].append(num)
 
-        return res
+        tot = sum(nums)
+        remove = float("inf")
+        if tot % 3 == 0:
+            remove = 0
+        elif tot % 3 == 1:
+            if len(d[1]) >= 1:
+                remove = min(remove, d[1][-1])
+            if len(d[2]) >= 2:
+                remove = min(remove, d[2][-2] + d[2][-1])
+        else:
+            if len(d[1]) >= 2:
+                remove = min(remove, d[1][-2] + d[1][-1])
+            if len(d[2]) >= 1:
+                remove = min(remove, d[2][-1])
+
+        return tot - remove
