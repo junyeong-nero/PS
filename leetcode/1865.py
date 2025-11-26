f3fb57eb457356878dee8bee9b85d644447725ed
junyeong-nero <magicwho@unist.ac.kr>
2025-11-26 11:59:14 +0900

    [solve] 1865 with stacks & prefix sum

--- leetcode/1865.py
+++ leetcode/1865.py
@@ -1,20 +1,37 @@
 class Solution:
     def maxSumMinProduct(self, nums: List[int]) -> int:
-
         n = len(nums)
-        MOD = 10**9 + 7
-        prefix_sum = [0]
-        for num in nums:
-            prefix_sum.append(prefix_sum[-1] + num)
+        left_bound, right_bound = [0] * n, [0] * n
+        st = []
+        for i in range(0, n):
+            while st and nums[st[-1]] >= nums[i]:
+                st.pop()
+            if len(st) > 0:
+                left_bound[i] = st[-1] + 1
+            else:
+                left_bound[i] = 0
+            st.append(i)
 
-        # prefix_sum[i] : sum(nums[:i])
+        st = []
+        for i in range(n - 1, -1, -1):
+            while st and nums[st[-1]] >= nums[i]:
+                st.pop()
+            if len(st) > 0:
+                right_bound[i] = st[-1] - 1
+            else:
+                right_bound[i] = n - 1
+            st.append(i)
 
-        res = 0
+        preSum = [0] * (n + 1)
         for i in range(n):
-            min_value = nums[i]
-            for j in range(i + 1, n + 1):
-                sum_value = prefix_sum[j] - prefix_sum[i]
-                min_value = min(min_value, nums[j - 1])
-                res = max(res, sum_value * min_value % MOD)
+            preSum[i + 1] = preSum[i] + nums[i]
 
-        return res
+        def getSum(left, right):  # left, right inclusive
+            return preSum[right + 1] - preSum[left]
+
+        maxProduct = 0
+        for i in range(n):
+            maxProduct = max(
+                maxProduct, nums[i] * getSum(left_bound[i], right_bound[i])
+            )
+        return maxProduct % 1000_000_007
