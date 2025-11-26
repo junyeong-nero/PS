e2fac911afd43e64ca15106ff1f72c80bcaf6a88
junyeong-nero <magicwho@unist.ac.kr>
2025-11-26 16:01:35 +0900

    [TLE] Simplified BFS

--- leetcode/3154.py
+++ leetcode/3154.py
@@ -5,19 +5,17 @@ class Solution:
             return cur + 2**jump
 
         res = 0
-        q = deque([(1, 0, True)])
-        d = defaultdict(int)
+        q = deque([(1, 0), (0, 0)])
 
         while q:
-            # print(q)
-            cur, jump, is_backward = q.popleft()
-            d[cur] += 1
+            cur, jump = q.popleft()
+            if cur == k:
+                res += 1
+            if cur > k:
+                continue
 
-            next_ = get_position(cur, jump)
-            if next_ <= 2 * k + 1:
-                q.append((next_, jump + 1, True))
-            if is_backward and cur > 0:
-                q.append((cur - 1, jump, False))
+            temp = get_position(cur, jump)
+            q.append((temp, jump + 1))
+            q.append((temp - 1, jump + 1))
 
-        # print(d)
-        return d[k]
+        return res
