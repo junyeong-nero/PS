cc7606fa19a88d8fc58d40fa69545d893e0089ce
junyeong-nero <magicwho@unist.ac.kr>
2025-11-26 16:15:09 +0900

    [solve] 3154 with DFS

--- leetcode/3154.py
+++ leetcode/3154.py
@@ -4,18 +4,19 @@ class Solution:
         def get_position(cur, jump):
             return cur + 2**jump
 
-        res = 0
-        q = deque([(1, 0), (0, 0)])
-
-        while q:
-            cur, jump = q.popleft()
+        @cache
+        def dfs(cur, jump):
+            # print(cur, jump)
+            if cur > k:
+                return 0
+            res = 0
             if cur == k:
                 res += 1
-            if cur > k:
-                continue
 
             temp = get_position(cur, jump)
-            q.append((temp, jump + 1))
-            q.append((temp - 1, jump + 1))
+            res += dfs(temp, jump + 1)
+            res += dfs(temp - 1, jump + 1)
+            return res
 
+        res = dfs(0, 0) + dfs(1, 0)
         return res
