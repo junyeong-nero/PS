4e49a7ca02500d4cf4ebfa9fc929a34c4b6ec686
junyeong-nero <magicwho@unist.ac.kr>
2025-11-27 09:51:34 +0900

    [solve] 3381 with prefix sum

--- leetcode/3381.py
+++ leetcode/3381.py
@@ -1,20 +1,37 @@
 class Solution:
     def maxSubarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        value = n // k
-
-        d = defaultdict(int)
-        res = -float("inf")
-        for i in range(value):
-            d[i] = sum(nums[: k * (i + 1)])
-            res = max(res, d[i])
+        cur = sum(nums[:k])
+        arr = [cur]
 
         for i in range(n - k):
-            for j in range(value):
-                if (i + k * (j + 1)) >= n:
-                    continue
-                d[j] -= nums[i]
-                d[j] += nums[i + k * (j + 1)]
-                res = max(res, d[j])
+            cur -= nums[i]
+            cur += nums[i + k]
+            arr.append(cur)
 
+        m = len(arr)
+        prefix = defaultdict(list)
+        for i in range(k):
+            if not prefix[i]:
+                prefix[i].append(0)
+            for j in range(n // k):
+                if i + j * k >= m:
+                    break
+                prefix[i].append(prefix[i][-1] + arr[i + j * k])
+
+        def get_maximum(prefix_arr):
+            res = -float("inf")
+            if len(prefix_arr) <= 1:
+                return res
+            cur_min = prefix_arr[0]
+            for num in prefix_arr[1:]:
+                res = max(res, num - cur_min)
+                cur_min = min(cur_min, num)
+            return res
+
+        # print(prefix)
+
+        temp = [get_maximum(arr) for arr in prefix.values()]
+        # print(temp)
+        res = max(temp)
         return res
