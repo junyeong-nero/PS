6395af26b09ce10b7250f88464c62b05b339d062
junyeong-nero <magicwho@unist.ac.kr>
2025-11-28 16:40:28 +0900

    [optimized] 424

--- leetcode/424.py
+++ leetcode/424.py
@@ -1,29 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        n = len(s)
+        left = 0
+        max_count = 0
+        max_length = 0
+        count = defaultdict(int)
+        for right in range(len(s)):
+            count[s[right]] += 1
+            max_count = max(max_count, count[s[right]])
+            while (right - left + 1) - max_count > k:  # window_size - max_count
+                count[s[left]] -= 1
+                left += 1
 
-        def check(counter):
-            common = counter.most_common()
-            if len(common) <= 1:
-                return sum([0] + [value for key, value in common])
+            max_length = max(max_length, (right - left + 1))
 
-            out = sum([value for key, value in common[1:]])
-            if out > k:
-                return -1
-            return sum([value for key, value in common])
-
-        i = 0
-        j = 0
-        res = 0
-        counter = Counter()
-        while i <= j and j < n:
-            if (temp := check(counter)) >= 0:
-                counter[s[j]] += 1
-                j += 1
-                res = max(res, temp)
-            else:
-                counter[s[i]] -= 1
-                i += 1
-
-        res = max(res, check(counter))
-        return res
+        return max_length
