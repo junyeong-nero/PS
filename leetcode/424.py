33fb4b91689e28abe560df00b01631f10127b8f8
junyeong-nero <magicwho@unist.ac.kr>
2025-11-28 16:38:55 +0900

    [solve] 424

--- leetcode/424.py
+++ leetcode/424.py
@@ -2,32 +2,28 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         n = len(s)
 
-        prefix = [Counter()]
-        for char in s:
-            prefix.append(prefix[-1] + Counter(char))
-        # print(prefix)
-
         def check(counter):
             common = counter.most_common()
-            # print(common)
             if len(common) <= 1:
-                return sum([value for key, value in common])
+                return sum([0] + [value for key, value in common])
 
-            out = 0
-            for key, value in common[1:]:
-                out += value
-
+            out = sum([value for key, value in common[1:]])
             if out > k:
                 return -1
             return sum([value for key, value in common])
 
-        res = float("-inf")
-        for i in range(1, n + 1):
-            for j in range(i):
-                counter = prefix[i] - prefix[j]
-                temp = check(counter)
-                if temp > 0:
-                    res = max(res, temp)
-                    break
+        i = 0
+        j = 0
+        res = 0
+        counter = Counter()
+        while i <= j and j < n:
+            if (temp := check(counter)) >= 0:
+                counter[s[j]] += 1
+                j += 1
+                res = max(res, temp)
+            else:
+                counter[s[i]] -= 1
+                i += 1
 
+        res = max(res, check(counter))
         return res
