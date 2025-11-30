a66ccdc8b5953a904417cd99dc249156a1cd1dfd
junyeong-nero <magicwho@unist.ac.kr>
2025-11-30 09:54:52 +0900

    [failed] 1590

---
+++ leetcode/1590.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        n = len(nums)
+        mod = sum(nums) % p
+        if mod == 0:
+            return 0
+
+        print(mod)
+        # find sum(subarray) % p = mod
+
+        prefix = [0]
+        for num in nums:
+            prefix.append(prefix[-1] + num)
+
+        res = float("inf")
+        d = [[] for _ in range(p)]
+        for i in range(n + 1):
+            temp = prefix[i] % p
+            # prefix[i] - prefix[j] = mod
+            # prefix[j] = (preifx[i] - mod + p) % p
+
+            j = (temp - mod + p) % p
+            if d[j]:
+                res = min(res, i - d[j][-1])
+            d[temp].append(i)
+
+        return -1 if res >= n else res
