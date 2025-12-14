3fff28526e264bb97c3f72840c316930b743661d
junyeong-nero <magicwho@unist.ac.kr>
2025-12-14 15:10:53 +0900

    [optimize] 498

--- leetcode/498.py
+++ leetcode/498.py
@@ -2,17 +2,12 @@ class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         m, n = len(mat), len(mat[0])
         res = []
-        for level in range(m + n - 1):
-            if level % 2 == 0:
-                temp = range(level, -1, -1)
-            else:
-                temp = range(level + 1)
+        d = defaultdict(list)
+        for x in range(m):
+            for y in range(n):
+                d[x + y].append(mat[x][y])
 
-            # print(level)
-            for x in temp:
-                y = level - x
-                if 0 <= x < m and 0 <= y < n:
-                    # print(x, y)
-                    res.append(mat[x][y])
+        for level in d:
+            res += d[level][::-1] if level % 2 == 0 else d[level]
 
         return res
