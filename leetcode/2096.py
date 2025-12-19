06783b02a1cf9b6fa51327ed09dcfbe837a7f8a4
junyeong-nero <magicwho@unist.ac.kr>
2025-12-19 16:39:18 +0900

    [optimized] 2096

--- leetcode/2096.py
+++ leetcode/2096.py
@@ -1,36 +1,18 @@
 class Solution:
     def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
         m, n = len(grid), len(grid[0])
-        MOD = 12345
+        prefix, suffix = [1], [1]
 
-        columns = []
-        counter = Counter()
-        x0, y0 = -1, -1
-        value = 1
+        for i in range(m):
+            for j in range(n):
+                prefix.append((prefix[-1] * grid[i][j]) % 12345)
 
-        for x in range(m):
-            for y in range(n):
-                cur = grid[x][y] % MOD
-                counter[cur] += 1
-                if cur == 0:
-                    x0, y0 = x, y
-                else:
-                    value *= grid[x][y]
-                    # value = value % MOD
+        for i in reversed(range(m)):
+            for j in reversed(range(n)):
+                suffix.append((suffix[-1] * grid[i][j]) % 12345)
 
-        if counter[0] >= 2:
-            # return all zero grids
-            return [[0] * n for _ in range(m)]
-
-        if counter[0] >= 1:
-            # return all zero grids excepts x0, y0
-            grid = [[0] * n for _ in range(m)]
-            grid[x0][y0] = value % MOD
-            return grid
-
-        for x in range(m):
-            for y in range(n):
-                grid[x][y] = value // grid[x][y]
-                grid[x][y] = grid[x][y] % MOD
+        for i, j in product(range(m), range(n)):
+            k = i * n + j
+            grid[i][j] = (prefix[k] * suffix[-k - 2]) % 12345
 
         return grid
