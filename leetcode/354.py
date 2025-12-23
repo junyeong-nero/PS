d296d2794e23f74f7b57f525105b5499d7dd70ca
junyeong-nero <magicwho@unist.ac.kr>
2025-12-23 16:42:14 +0900

    [MLE] 354 with DP

--- leetcode/354.py
+++ leetcode/354.py
@@ -1,21 +1,22 @@
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        if not envelopes:
+            return 0
+
+        # 1. 가로 길이 기준 오름차순 정렬
+        envelopes.sort()
         n = len(envelopes)
-        envelopes = sorted(envelopes)
-        print(envelopes)
 
-        @cache
-        def dfs(cur, value):
-            w, h = envelopes[cur] if cur >= 0 else [0, 0]
-            start = bisect_left(envelopes, w + 1, key=lambda x: x[0])
-            start = max(cur + 1, start)
+        # 2. dp[i]: i번째 봉투를 가장 바깥으로 했을 때 최대 개수
+        dp = [1] * n
 
-            res = value
-            for i in range(start, n):
-                if envelopes[i][0] > w and envelopes[i][1] > h:
-                    res = max(res, dfs(i, value + 1))
+        for i in range(n):
+            for j in range(i):
+                # i번째 봉투가 j번째 봉투를 담을 수 있는지 확인
+                if (
+                    envelopes[j][0] < envelopes[i][0]
+                    and envelopes[j][1] < envelopes[i][1]
+                ):
+                    dp[i] = max(dp[i], dp[j] + 1)
 
-            return res
-
-        res = dfs(-1, 0)
-        return res
+        return max(dp)
