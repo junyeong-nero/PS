97a815d426c849ba1f8e63bdeaacbaa1a68dcb00
junyeong-nero <magicwho@unist.ac.kr>
2026-01-08 10:57:55 +0900

    [failed] 754 with BFS

--- leetcode/754.py
+++ leetcode/754.py
@@ -3,17 +3,33 @@ class Solution:
 
         bound = abs(target) * 2
 
-        @cache
-        def dfs(cur=0, steps=1):
-            if not (-bound < cur < bound):
-                return float("inf")
-            if cur == target:
-                return 0
+        def in_boundary(num):
+            return -bound < num < bound
 
-            res = float("inf")
-            res = min(res, 1 + dfs(cur + steps, steps + 1))
-            res = min(res, 1 + dfs(cur - steps, steps + 1))
-            return res
+        def bfs(cur=0):
+            q = deque([(cur, 0)])
+            while q:
+                tar, step = q.popleft()
+                if tar == target:
+                    return step - 1
+                if in_boundary(tar + step):
+                    q.append((tar + step, step + 1))
+                if in_boundary(tar - step):
+                    q.append((tar - step, step + 1))
 
-        temp = dfs()
+            return -1
+
+        # @cache
+        # def dfs(cur=0, steps=1):
+        #     if not (-bound < cur < bound):
+        #         return float("inf")
+        #     if cur == target:
+        #         return 0
+
+        #     res = float("inf")
+        #     res = min(res, 1 + dfs(cur + steps, steps + 1))
+        #     res = min(res, 1 + dfs(cur - steps, steps + 1))
+        #     return res
+
+        temp = bfs()
         return temp
